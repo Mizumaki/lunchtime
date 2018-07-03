2ff5b91c0aae3b2011cec74c8b233d20500a164c
Mizumaki <statham.programming@gmail.com>
2018-07-03 16:58:38 +0900

    add status

--- src/nomad/generateInfo.js
+++ src/nomad/generateInfo.js
@@ -20,14 +20,39 @@ export default class FormatInfo {
       bsh: this.getBsh(),
       has_wifi: this.getWifi(),
       has_charge: this.getCharge(),
+      status: this.getStatus(),
     }
   }
   // 営業中か否か
   getStatus() {
     const now = new Date(); // 現在時刻を取得
-    const today_bsh = this.data.business_hours[now.getDay()] // now.getDay()で曜日を呼び出し、それに対応するbshを取り出す
+    const today_bsh = this.data.business_hours[6] // now.getDay()で曜日を呼び出し、それに対応するbshを取り出す
     // 表示のパターン　営業中　本日の営業時間　06:00~26:00、準備中　本日の営業時間　06:00~26:00、（45m ~ 0前まで）営業終了まで残り約30分　本日の営業時間　06:00~26:00、（1h15m ~ 45m前まで）営業終了まで残り約1時間　本日の営業時間　06:00~26:00
     // 準備中　本日の営業時間　定休日
+    // 6 ~ 2 , 7 ~ 23
+    // start_hour <= now_hour , 
+    const start_time = new Date(today_bsh.start_time)
+    const start_hour = start_time.getHours()
+    const start_min = start_time.getMinutes()
+    const end_time = new Date(today_bsh.end_time)
+    const end_hour = end_time.getHours()
+    const end_min = end_time.getMinutes()
+    const now_hour = now.getHours()
+    const now_min = now.getMinutes()
+
+    const status = (() => {
+      if (now_hour === start_hour) {
+        return start_min <= now_min ? ("営業中") : ("営業時間外");
+      } else if (now_hour === end_hour) {
+        return now_min < end_min ? ("営業中") : ("営業時間外");
+      } else if (end_hour > 9) { // 深夜営業でない場合
+        return start_hour < now_hour && now_hour < end_hour ? ("営業中") : ("営業時間外");
+      } else { // 深夜営業の場合
+        return start_hour < now_hour || now_hour < end_hour ? ("営業中") : ("営業時間外"); // now_hourは必ず24以下なので
+      } 
+    })();
+    console.log(status)
+    return status
   }
 
   getChainName() {
